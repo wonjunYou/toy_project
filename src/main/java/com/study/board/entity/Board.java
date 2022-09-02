0dde140521709cd92b56c196ba69767bb0edac3e
wonjunYou <youwjune@gmail.com>
2022-09-02 23:13:08 +0900

    [feat] 게시글 작성한 일자 column 추가

--- src/main/java/com/study/board/entity/Board.java
+++ src/main/java/com/study/board/entity/Board.java
@@ -3,14 +3,17 @@ package com.study.board.entity;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Table(name = "board")
+@EntityListeners(AuditingEntityListener.class)
 public class Board {
 
     @Id
@@ -25,13 +28,24 @@ public class Board {
 
     private String filepath;
 
+    @CreatedDate
+    @Column(name = "createdTime", updatable = false)
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    @Column(name = "updatedTime", updatable = false)
+    private LocalDateTime updatedTime;
+
     @Builder
-    public Board(Integer id, String title, String content, String filename, String filepath) {
+    public Board(Integer id, String title, String content, String filename, String filepath,
+                 LocalDateTime createdTime, LocalDateTime updatedTime) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.filename = filename;
         this.filepath = filepath;
+        this.createdTime = createdTime;
+        this.updatedTime = updatedTime;
     }
 
     public Board() {
